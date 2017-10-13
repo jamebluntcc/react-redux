6ab870f64a75f7c3cdca35c49a6a8ee7ade48f6a
chencheng <291552579@qq.com>
2017-10-13 16:48:27 +0800

    react-redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import ControlPanel from './view/ControlPanel.js';
+import store from './Store.js';
 ReactDOM.render(
-  <ControlPanel />,
+  <Provider store={store}>
+    <ControlPanel />
+  </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
